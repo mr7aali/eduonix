a4e53c35594c94e3fef512488331228734c49471
mr7aali <mr7aali@gmail.com>
2022-10-27 02:54:33 +0600

    code afer instal tailwind

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Error from "../../component/ErrorPage/Error";
 import Home from "../../component/Home/Home";
+import Login from "../../HandleUser/Login/Login";
+import Register from "../../HandleUser/Register/Register";
 import Main from "../../layout/Main";
 
 export const routes = createBrowserRouter([
@@ -11,7 +13,15 @@ export const routes = createBrowserRouter([
             {
                 path:'/',
                 element:<Home></Home>
-            }
+            },
+            {
+                path:'/login',
+                element:<Login></Login>
+            },
+            {
+                path:'/register',
+                element:<Register></Register>
+            },
         ]
     },
     {
--- src/component/Home/Carousels/Carousel.js
+++ src/component/Home/Carousels/Carousel.js
@@ -5,6 +5,7 @@ import pic2 from "../../../assests/pic2.jpg"
 import pic3 from "../../../assests/pic3.jpg"
 import './Carousel.css'
 import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
 const HomeCarousel = () => {
     return (
         <div>
@@ -16,10 +17,10 @@ const HomeCarousel = () => {
                         alt="First slide"
                     />
                     <Carousel.Caption>
-                    
+
                         <h1 className='m-5 text-dark'>Start Learning a Digital Skill Now</h1>
                         <p className='mb-5 fs-5'>World is changing rapidly and so is the technology. Learn a new technology to build a better future for you and for the world. Choose to learn web development, app development and many more</p>
-                        <Button className='m-5 p-3 fs-5' variant="primary">Get Start</Button>
+                        <Link to='/login'><Button className='m-5 p-3 fs-5' variant="primary">Get Start</Button></Link>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
@@ -32,7 +33,7 @@ const HomeCarousel = () => {
                     <Carousel.Caption>
                         <h1 className='m-5 text-dark'>Transform your life through education</h1>
                         <p className='mb-5 fs-5'>Better education opens up a new wave of opportunities and it is now more relevant than ever in the times we live in where technology and education ensure that opportunities are no longer limited by geography.</p>
-                        <Button className='m-5 p-3 fs-5' variant="primary">Get Start</Button>
+                        <Link to='/login'><Button className='m-5 p-3 fs-5' variant="primary">Get Start</Button></Link>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
@@ -45,9 +46,10 @@ const HomeCarousel = () => {
                     <Carousel.Caption>
                         <h1 className='m-5 text-dark'>Learn without limits</h1>
                         <p className='mb-5 fs-5'>
-                        Start, switch, or advance your career with more than 5,200 courses, Professional Certificates, and degrees from world-class universities and companies.
+                            Start, switch, or advance your career with more than 5,200 courses, Professional Certificates, and degrees from world-class universities and companies.
                         </p>
-                        <Button className='m-5 p-3 fs-5' variant="primary">Get Start</Button>
+                        <Link to='/login'><Button className='m-5 p-3 fs-5' variant="primary">Get Start</Button></Link>
+
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
@@ -55,4 +57,4 @@ const HomeCarousel = () => {
     );
 };
 
-export default HomeCarousel ;
+export default HomeCarousel;
