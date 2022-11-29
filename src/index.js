acceb2377412a346f911e117d2b8bb22e3c85df7
mr7aali <mr7aali@gmail.com>
2022-11-30 02:09:18 +0600

    d

--- src/index.js
+++ src/index.js
@@ -3,16 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import UserContext from './context/UserContext';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <UserContext>
-      <App />
-    </UserContext>
+    <App />
   </React.StrictMode>
 );
 
