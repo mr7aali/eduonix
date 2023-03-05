aef63d7da56b926fe16a39757713ac98f507b08d
mr7aali <mr7aali@gmail.com>
2023-03-05 20:24:10 +0600

    add header

--- src/contetent/Header/Header.js
+++ src/contetent/Header/Header.js
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../Context/AuthProvider';
-import ToggleSwitch from '../Toggole/ToggleSwitch';
 import './Header.css'
 
 
@@ -16,118 +15,43 @@ const Header = () => {
 
     return (
         //max-w-screen-2xl mx-auto
-        <div className=' bg-base-300 full-header'>
-
-            <div className="navbar mx-auto max-w-screen-xl	">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                        </label>
-                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                           
-
-
-                            <Link className=' btn btn-outline mb-5' to='course' >Course</Link>
-                            <Link className=' btn btn-outline mb-5' to='home'>Faq</Link>
-                            <Link className=' btn btn-outline mb-5' to='blog'>Blog</Link>
-
-
-
-
-
-                            {
-                                user?.email ?
-                                    <Link to='/resister' onClick={handleLogOut} className='btn mt-5'>Log Out </Link>
-
-                                    :
-                                    <>
-
-
-                                        <Link to='/login' className="btn mt-5">Log In</Link>
-                                        <Link to='/resister' className='btn mt-5'>Resister </Link>
-                                    </>
-                            }
-
-
-                        </ul>
+        <>
+        
+            <header>
+                <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+                    <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+                        <a href="https://flowbite.com" class="flex items-center">
+                            <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+                            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                        </a>
+                        <div class="flex items-center lg:order-2">
+                            <Link to="/login" class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log in</Link>
+                            <Link to="/resister" class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">Get started</Link>
+                            <button data-collapse-toggle="mobile-menu-2" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
+                                <span class="sr-only">Open main menu</span>
+                                <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+                                <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                            </button>
+                        </div>
+                        <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
+                            <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                                <li>
+                                    <Link to="/" class="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white" aria-current="page">Course</Link>
+                                </li>
+                                <li>
+                                    <Link to="/home" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Faq</Link>
+                                </li>
+                                <li>
+                                    <Link to="/blog" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Blog</Link>
+                                </li>
+                                
+                                
+                            </ul>
+                        </div>
                     </div>
-                    <Link to='/' className="btn btn-ghost normal-case  font-bold text-xl">best Sch <span className='text-primary font-bold'>oo</span> l</Link>
-                </div>
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal p-0">
-
-
-                        <li className='text-xl'><Link to='course'>Course</Link></li>
-                        <li className='text-xl'><Link to='home'>Faq</Link></li>
-                        <li className='text-xl'><Link to='blog'>Blog</Link></li>
-                    </ul>
-                </div>
-
-                <div className="navbar-end">
-
-                    <div className='hidden sm:flex'>
-                        <React.Fragment>
-                            <ToggleSwitch label=" " />
-                        </React.Fragment>
-                    </div>
-
-
-                    {
-                        user ?
-                            <>
-
-                                <div className='header-name-img-container'>
-
-
-
-                                    <div id='left' className='m-2 name-container '>
-                                        <div className="font-bold">{user?.displayName}</div>
-                                        <div className="text-sm opacity-50">{user?.email}</div>
-                                    </div>
-
-
-
-
-
-
-                                    <div className="avatar just-img-div">
-                                        <div className="mask mask-squircle w-12 h-12 mr-5  header-img ">
-
-
-                                            {
-                                                user.photoURL ?
-                                                    <img src={user?.photoURL} alt="Avatar Tailwind CSS Component" />
-                                                    :
-                                                    <img src={'https://i.ibb.co/vqPRQzM/depositphotos-69759195-stock-illustration-male-person-silhouette.webp'} alt="Avatar Tailwind CSS Component" />
-                                            }
-
-                                        </div>
-                                    </div>
-
-
-                                </div>
-
-                                <Link to='/resister' onClick={handleLogOut} className='btn'>Log Out </Link>
-                            </>
-                            :
-                            <>
-                                <Link to='/login' className="btn btn-outline mx-5">Log In</Link>
-                                <Link to='/resister' className='btn'>Resister </Link>
-                            </>
-
-                    }
-
-
-
-                </div>
-
-
-
-            </div>
-
-            <hr />
-        </div>
+                </nav>
+            </header>
+        </>
     );
 };
 
--- src/contetent/Home/Home.js
+++ src/contetent/Home/Home.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import Banner2 from '../Banner2/Banner2';
 import Banner from './Bannr/Banner';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
-            {/* <Banner2></Banner2> */}
+           
         </div>
     );
 };
